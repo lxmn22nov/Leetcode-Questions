d0f5af700c045c5b6e048b5c73f17dbf3ce904ae
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-08-05 23:14:31 +0530

    Initial commit

---
+++ Medium/Permutations/Permutations.java
@@ -0,0 +1,28 @@
+/*Given an array nums of distinct integers, return all the possible permutations. 
+You can return the answer in any order. 
+*/
+class Permutations {
+      public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            Set<Integer> visited = new HashSet<>();
+            permute(nums, result, new ArrayList<>(), visited);
+            return result;
+      }
+        
+      private void permute(int[] nums, List<List<Integer>> result, List<Integer> curr, Set<Integer> visited) {
+            if (curr.size() == nums.length) {
+                  result.add(new ArrayList<>(curr));
+                  return;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                  if (visited.contains(nums[i])) {
+                        continue;
+                  }
+                  visited.add(nums[i]);
+                  curr.add(nums[i]);
+                  permute(nums, result, curr, visited);
+                  visited.remove(nums[i]);
+                  curr.remove(curr.size() - 1);
+            }
+      }
+}
