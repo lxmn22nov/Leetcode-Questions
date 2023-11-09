27b8514f42c1ed48c922ef7883391236ba1a0c8c
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-09 13:34:48 +0530

    Added new folder intro to pandas.

---
+++ Introduction-To-Pandas/Easy/CreateAdataframeFromList.py
@@ -0,0 +1,24 @@
+"""
+Write a solution to create a DataFrame from a 2D list called student_data. 
+This 2D list contains the IDs and ages of some students.
+The DataFrame should have two columns, student_id and age,
+and be in the same order as the original 2D list.
+
+The result format is in the following example.
+
+Example 1:
+Input:
+student_data:
+[
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+"""
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    col_names = ['student_id', 'age']
+    result = pd.DataFrame(student_data, columns = col_names)
+    return result
