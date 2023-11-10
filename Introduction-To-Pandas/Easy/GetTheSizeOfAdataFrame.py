d94cdd948dcdab9be071b241a4891fd2dad7d72e
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-10 10:30:29 +0530

    Added question into Easy.

---
+++ Introduction-To-Pandas/Easy/GetTheSizeOfAdataFrame.py
@@ -0,0 +1,20 @@
+"""
+DataFrame players:
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| player_id   | int    |
+| name        | object |
+| age         | int    |
+| position    | object |
+| ...         | ...    |
++-------------+--------+
+Write a solution to calculate and display the number of rows and columns of players.
+Return the result as an array:
+[number of rows, number of columns]
+The result format is in the following example.
+"""
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
