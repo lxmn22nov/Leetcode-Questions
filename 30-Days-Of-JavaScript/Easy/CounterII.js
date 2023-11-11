73c7ec894267715fdfb9a5faafb4e4a7fc3d102b
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-11 09:08:05 +0530

    Added question to Easy js.

---
+++ 30-Days-Of-JavaScript/Easy/CounterII.js
@@ -0,0 +1,43 @@
+/* PROBLEM:
+Write a function createCounter. It should accept an initial integer init. 
+It should return an object with three functions.
+The three functions are:
+increment() increases the current value by 1 and then returns it.
+decrement() reduces the current value by 1 and then returns it.
+reset() sets the current value to init and then returns it.
+
+Example 1:
+Input: init = 5, calls = ["increment","reset","decrement"]
+Output: [6,5,4]
+Explanation:
+const counter = createCounter(5);
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
+*/
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment() {
+            return ++count;
+        },
+        decrement() {
+            return --count;
+        },
+        reset() {
+            count = init;
+            return count;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
