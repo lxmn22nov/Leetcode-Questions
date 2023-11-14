b0d4bd097c9e49994410255ae3444bd734e8b32a
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-14 13:37:05 +0530

    Added question to Easy.

---
+++ 30-Days-Of-JavaScript/Easy/ApplyTransformOverEachElementInArray.js
@@ -0,0 +1,32 @@
+/*
+Given an integer array arr and a mapping function fn, return a new array with a transformation 
+applied to each element.
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+Please solve it without the built-in Array.map method.
+
+Example 1:
+Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+Output: [2,3,4]
+Explanation:
+const newArray = map(arr, plusone); // [2,3,4]
+The function increases each value in the array by one. 
+
+Example 2:
+Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+Output: [1,3,5]
+Explanation: The function increases each value by the index it resides in.
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    var array = [];
+    for(i = 0; i < arr.length; i++)
+    {
+        array[i] = fn(arr[i], i);
+    }
+    return array;
+};
