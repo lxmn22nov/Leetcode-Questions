0d33e76c6167bf450570fb4377d482c26ac8854c
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-15 10:37:49 +0530

    Added question to Easy.

---
+++ Introduction-To-Pandas/Easy/DropMissingData.py
@@ -0,0 +1,38 @@
+"""
+DataFrame students
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| student_id  | int    |
+| name        | object |
+| age         | int    |
++-------------+--------+
+There are some rows having missing values in the name column.
+Write a solution to remove the rows with missing values.
+The result format is in the following example.
+
+Example 1:
+Input:
++------------+---------+-----+
+| student_id | name    | age |
++------------+---------+-----+
+| 32         | Piper   | 5   |
+| 217        | None    | 19  |
+| 779        | Georgia | 20  |
+| 849        | Willow  | 14  |
++------------+---------+-----+
+Output:
++------------+---------+-----+
+| student_id | name    | age |
++------------+---------+-----+
+| 32         | Piper   | 5   |
+| 779        | Georgia | 20  | 
+| 849        | Willow  | 14  | 
++------------+---------+-----+
+Explanation: 
+Student with id 217 havs empty value in the name column, so it will be removed.
+"""
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame(students)[students['name'].notnull()]
