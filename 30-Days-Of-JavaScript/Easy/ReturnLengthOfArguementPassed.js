cec062fcbd391d5a13d0d46143682d78eda51abc
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-17 14:12:17 +0530

    Added question to Easy.

---
+++ 30-Days-Of-JavaScript/Easy/ReturnLengthOfArguementPassed.js
@@ -0,0 +1,25 @@
+/*
+Write a function argumentsLength that returns the count of arguments passed to it.
+
+Example 1:
+Input: args = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+One value was passed to the function so it should return 1.
+
+Example 2:
+Input: args = [{}, null, "3"]
+Output: 3
+*/
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+	return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
