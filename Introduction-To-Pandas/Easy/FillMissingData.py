ba5717ef1b3f3357299363c483e61a64b8fb167f
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-19 17:57:23 +0530

    Added question to Easy.

---
+++ Introduction-To-Pandas/Easy/FillMissingData.py
@@ -0,0 +1,38 @@
+"""
+DataFrame products
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| name        | object |
+| quantity    | int    |
+| price       | int    |
++-------------+--------+
+Write a solution to fill in the missing value as 0 in the quantity column.
+The result format is in the following example.
+
+Example 1:
+Input:+-----------------+----------+-------+
+| name            | quantity | price |
++-----------------+----------+-------+
+| Wristwatch      | None     | 135   |
+| WirelessEarbuds | None     | 821   |
+| GolfClubs       | 779      | 9319  |
+| Printer         | 849      | 3051  |
++-----------------+----------+-------+
+Output:
++-----------------+----------+-------+
+| name            | quantity | price |
++-----------------+----------+-------+
+| Wristwatch      | 0        | 135   |
+| WirelessEarbuds | 0        | 821   |
+| GolfClubs       | 779      | 9319  |
+| Printer         | 849      | 3051  |
++-----------------+----------+-------+
+Explanation: 
+The quantity for Wristwatch and WirelessEarbuds are filled by 0.
+"""
+import pandas as pd
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products["quantity"] = products["quantity"].fillna(0)
+    return products
