f1ba69a2882206c51bb4e564619205be55ae8a25
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-23 23:11:04 +0530

    Added question to Easy.

---
+++ Java-Solutions/Easy/SearchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,44 @@
+/*
+Given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not, return the index where it would be 
+if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+*/
+package Easy.SearchInsertPosition;
+
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 5;
+        System.out.println("Search Insert: " + searchInsert(nums, target));
+    }
+}
