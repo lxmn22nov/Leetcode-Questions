324c27556a4eb09343ba92df4bed51dd54129a29
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-29 13:29:05 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/DeleteTheMiddleNodeOfTheLinkedList/DeleteTheMiddleNodeOfTheLinkedList.java
@@ -0,0 +1,44 @@
+/*
+You are given the head of a linked list. Delete the middle node, and return the head of the 
+modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based 
+indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+
+Example 1:
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
+ */
+package Medium.DeleteTheMiddleNodeOfTheLinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class DeleteTheMiddleNodeOfTheLinkedList {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
