c40610caff79cfb2e5100328c2aa76512ff17e97
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-05 16:55:55 +0530

    Added question to Easy.

---
+++ Java-Solutions/Easy/CountOfMatchesInTournament/CountOfMatchesInTournament.java
@@ -0,0 +1,39 @@
+/*
+ * You are given an integer n, the number of teams in a tournament that has
+ * strange rules:
+ * 
+ * If the current number of teams is even, each team gets paired with another
+ * team. A total of
+ * n / 2 matches are played, and n / 2 teams advance to the next round.
+ * If the current number of teams is odd, one team randomly advances in the
+ * tournament, and the
+ * rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2
+ * + 1 teams advance
+ * to the next round.
+ * Return the number of matches played in the tournament until a winner is
+ * decided.
+ * 
+ * Example 1:
+ * Input: n = 7
+ * Output: 6
+ * Explanation: Details of the tournament:
+ * - 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
+ * - 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
+ * - 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+ * Total number of matches = 3 + 2 + 1 = 6.
+ */
+class CountOfMatchesInTournament {
+    public static int numberOfMatches(int n) {
+        int numOfMatches = 0;
+        while (n > 1) {
+            numOfMatches += n / 2;
+            n = n / 2 + n % 2;
+        }
+        return numOfMatches;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println("Number of matches: " + numberOfMatches(n));
+    }
+}
