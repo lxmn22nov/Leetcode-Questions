cc9cc10061d25175e69f71f3de9083cff6a794ab
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-18 14:36:57 +0530

    Added question to Easy.

---
+++ Java-Solutions/Easy/MaximumProductDifferenceBetweenTwoPairs/MaximumProductDifferenceBetweenTwoPairs.java
@@ -0,0 +1,27 @@
+/*
+The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], 
+nums[x]) and (nums[y], nums[z]) is maximized.
+Return the maximum such product difference.
+
+Example 1:
+Input: nums = [5,6,2,7,4]
+Output: 34
+Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+The product difference is (6 * 7) - (2 * 4) = 34.
+*/
+
+class MaximumProductDifferenceBetweenTwoPairs {
+    public static int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 5, 6, 2, 7, 4 };
+        System.out.println("Maximum product Difference: " + maxProductDifference(nums));
+    }
+}
+
+import java.util.Arrays;
