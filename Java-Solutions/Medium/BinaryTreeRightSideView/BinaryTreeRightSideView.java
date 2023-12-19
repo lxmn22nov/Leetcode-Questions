7fead8f9fb8762ed1a6ac6ca1ae289d7bb9405cb
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-19 20:48:03 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/BinaryTreeRightSideView/BinaryTreeRightSideView.java
@@ -0,0 +1,50 @@
+/*
+ * Given the root of a binary tree, imagine yourself standing on the right side
+ * of it, return the values of the nodes you can
+ * see ordered from top to bottom.
+ * 
+ * Example 1:
+ * Input: root = [1,2,3,null,5,null,4]
+ * Output: [1,3,4]
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class BinaryTreeRightSideView {
+    public static List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int rightMostValue = -1;
+            while (size-- > 0) {
+                TreeNode removed = queue.remove();
+                rightMostValue = removed.val;
+                if (removed.left != null) {
+                    queue.add(removed.left);
+                }
+                if (removed.right != null) {
+                    queue.add(removed.right);
+                }
+            }
+            result.add(rightMostValue);
+        }
+        return result;
+    }
+}
