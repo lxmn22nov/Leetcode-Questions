474480a6c0845e9e2591f2daed93380a670b97c5
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-12-20 17:41:55 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/DeleteNodeInBST/DeleteNodeInBST.java
@@ -0,0 +1,56 @@
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. 
+Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+
+Example 1:
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+ */
+package Medium.DeleteNodeInBST;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class DeleteNodeInBST {
+    public static TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null || root.right == null) {
+                return root.left == null ? root.right : root.left;
+            }
+            TreeNode inorderSuccessor = root.right;
+            while (inorderSuccessor.left != null) {
+                inorderSuccessor = inorderSuccessor.left;
+            }
+            root.val = inorderSuccessor.val;
+            root.right = deleteNode(root.right, inorderSuccessor.val);
+        }
+        return root;
+    }
+}
