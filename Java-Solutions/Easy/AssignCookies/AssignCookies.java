58766f889f666276e64a75beb2f0da7d3a23b974
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-01 23:08:55 +0530

    Added question to Easy.

---
+++ Java-Solutions/Easy/AssignCookies/AssignCookies.java
@@ -0,0 +1,48 @@
+
+/*
+ * Assume you are an awesome parent and want to give your children some cookies.
+ * But, you should give each
+ * child at most one cookie.
+ * Each child i has a greed factor g[i], which is the minimum size of a cookie
+ * that the child will be
+ * content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can
+ * assign the cookie j to the
+ * child i, and the child i will be content. Your goal is to maximize the number
+ * of your content children
+ * and output the maximum number.
+ * 
+ * Example 1:
+ * Input: g = [1,2,3], s = [1,1]
+ * Output: 1
+ * Explanation: You have 3 children and 2 cookies. The greed factors of 3
+ * children are 1, 2, 3.
+ * And even though you have 2 cookies, since their size is both 1, you could
+ * only make the child whose greed factor is 1 content.
+ * You need to output 1.
+ */
+import java.util.*;
+
+class AssignCookies {
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int index = 0;
+        int cnt = 0;
+
+        while (index < s.length && cnt < g.length) {
+            if (s[index] >= g[cnt]) {
+                cnt++;
+            }
+            index++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] g = { 1, 2, 3 };
+        int[] s = { 1, 1 };
+
+        System.out.println("Assigning Cookies: " + findContentChildren(g, s));
+    }
+}
