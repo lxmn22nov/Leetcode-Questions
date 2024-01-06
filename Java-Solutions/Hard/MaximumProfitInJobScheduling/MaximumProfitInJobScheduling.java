322cdde5bbd765408380edbed006bc796b138f6b
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-06 18:18:11 +0530

    Added question to Hard.

---
+++ Java-Solutions/Hard/MaximumProfitInJobScheduling/MaximumProfitInJobScheduling.java
@@ -0,0 +1,70 @@
+/*
+ * We have n jobs, where every job is scheduled to be done from startTime[i] to
+ * endTime[i],
+ * obtaining a profit of profit[i].
+ * You're given the startTime, endTime and profit arrays, return the maximum
+ * profit you can
+ * take such that there are no two jobs in the subset with overlapping time
+ * range.
+ * If you choose a job that ends at time X you will be able to start another job
+ * that starts
+ * at time X.
+ * 
+ * Example 1:
+ * Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+ * Output: 120
+ * Explanation: The subset chosen is the first and fourth job.
+ * Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+ */
+class MaximumProfitInJobScheduling {
+    public static int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int numJobs = profit.length; // Number of jobs
+        Job[] jobs = new Job[numJobs];
+
+        for (int i = 0; i < numJobs; ++i) {
+            jobs[i] = new Job(endTime[i], startTime[i], profit[i]);
+        }
+
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job.endTime));
+        int[] dp = new int[numJobs + 1];
+
+        for (int i = 0; i < numJobs; ++i) {
+            int endTimeValue = jobs[i].endTime;
+            int startTimeValue = jobs[i].startTime;
+            int profitValue = jobs[i].profit;
+
+            int latestNonConflictJobIndex = upperBound(jobs, i, startTimeValue);
+            dp[i + 1] = Math.max(dp[i], dp[latestNonConflictJobIndex] + profitValue);
+        }
+
+        return dp[numJobs];
+    }
+
+    private int upperBound(Job[] jobs, int endIndex, int targetTime) {
+        int low = 0;
+        int high = endIndex;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (jobs[mid].endTime <= targetTime) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    private static class Job {
+        int endTime;
+        int startTime;
+        int profit;
+
+        public Job(int endTime, int startTime, int profit) {
+            this.endTime = endTime;
+            this.startTime = startTime;
+            this.profit = profit;
+        }
+    }
+}
