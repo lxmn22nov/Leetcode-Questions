718a142f977ca8e90e958747031d72b010110734
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-26 13:55:13 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/OutOfBoundaryPaths/OutOfBoundaryPaths.java
@@ -0,0 +1,44 @@
+/*
+There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn]. 
+You are allowed to move the ball to one of the four adjacent cells in the grid (possibly out of 
+the grid crossing the grid boundary). You can apply at most maxMove moves to the ball.
+
+Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move 
+the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
+
+Example 1:
+Input: m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0
+Output: 6
+ */
+
+package Medium.OutOfBoundaryPaths;
+
+public class OutOfBoundaryPaths {
+    public int findPaths(int m, int n, int N, int x, int y) {
+        final int M = 1000000000 + 7;
+        int[][] dp = new int[m][n];
+        dp[x][y] = 1;
+        int count = 0;
+
+        for (int moves = 1; moves <= N; moves++) {
+            int[][] temp = new int[m][n];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == m - 1)
+                        count = (count + dp[i][j]) % M;
+                    if (j == n - 1)
+                        count = (count + dp[i][j]) % M;
+                    if (i == 0)
+                        count = (count + dp[i][j]) % M;
+                    if (j == 0)
+                        count = (count + dp[i][j]) % M;
+                    temp[i][j] = (((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M
+                            + ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M) % M;
+                }
+            }
+            dp = temp;
+        }
+        return count;
+    }
+}
