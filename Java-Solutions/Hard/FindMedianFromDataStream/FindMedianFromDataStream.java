871ffde9f0ac1a9d40977a376f808525ea3a4a1c
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-01-28 19:35:40 +0530

    Added question to Hard.

---
+++ Java-Solutions/Hard/FindMedianFromDataStream/FindMedianFromDataStream.java
@@ -0,0 +1,61 @@
+/*
+The median is the middle value in an ordered integer list. If the size of the list is even, 
+there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ */
+package Hard.FindMedianFromDataStream;
+
+public class FindMedianFromDataStream {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+        if (maxHeap.size() == minHeap.size()) {
+            minHeap.add(num);
+            maxHeap.add(minHeap.remove());
+        } else {
+            maxHeap.add(num);
+            minHeap.add(maxHeap.remove());
+        }
+
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+        return maxHeap.peek() / 1.0;
+    }
+}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
