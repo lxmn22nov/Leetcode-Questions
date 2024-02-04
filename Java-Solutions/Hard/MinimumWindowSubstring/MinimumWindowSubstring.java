9da4941d159c5a10b12ae7e60d8e617c78a357a6
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-02-04 20:41:40 +0530

    Added question to Hard.

---
+++ Java-Solutions/Hard/MinimumWindowSubstring/MinimumWindowSubstring.java
@@ -0,0 +1,45 @@
+/*
+Given two strings s and t of lengths m and n respectively, return the minimum window substring of 
+s such that every character in t (including duplicates) is included in the window. 
+If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+ */
+class MinimumWindowSubstring {
+    public static String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
+        }
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+
+        char[] chS = s.toCharArray();
+
+        while (end < chS.length) {
+            if (map[chS[end++]]-- > 0) {
+                count--;
+            }
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
+                }
+                if (map[chS[start++]]++ == 0) {
+                    count++;
+                }
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? new String() : new String(chS, startIndex, minLen);
+    }
+}
