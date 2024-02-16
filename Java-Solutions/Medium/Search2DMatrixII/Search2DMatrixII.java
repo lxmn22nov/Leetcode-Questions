3c9c5a1bf0fb133367a101b8c9c8bb6e8db3f6db
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-02-16 13:42:13 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/Search2DMatrixII/Search2DMatrixII.java
@@ -0,0 +1,28 @@
+package Medium.Search2DMatrixII;
+
+public class Search2DMatrixII {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+            return false;
+        }
+        int col = matrix[0].length - 1;
+        int row = 0;
+        while (col >= 0 && row <= matrix.length - 1) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+        int target = 5;
+        System.out.println("Search a 2D Matrix II: " + searchMatrix(matrix, target));
+    }
+}
