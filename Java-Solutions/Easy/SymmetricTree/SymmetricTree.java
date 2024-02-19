ef4aa1eeb3291d0016bd75646fe8a9078d19d368
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-02-19 21:01:07 +0530

    Added question to Easy.

---
+++ Java-Solutions/Easy/SymmetricTree/SymmetricTree.java
@@ -0,0 +1,53 @@
+/*
+Given the root of a binary tree, check whether it is a mirror of itself 
+(i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+ */
+package Easy.SymmetricTree;
+
+/* Definition for a binary tree node.*
+public class TreeNode {
+    *
+    int val;*
+    TreeNode left;*
+    TreeNode right;*
+
+    TreeNode() {
+    }*
+
+    TreeNode(int val) {
+        this.val = val;
+    }*
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }*
+} 
+*/
+public class SymmetricTree {
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private static boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+    }
+}
