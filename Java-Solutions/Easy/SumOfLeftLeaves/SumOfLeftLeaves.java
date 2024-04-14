05c2de4d48db22c068e3987e43268c52fba41ec4
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-04-14 19:20:06 +0530

    Added question to Easy.

---
+++ Java-Solutions/Easy/SumOfLeftLeaves/SumOfLeftLeaves.java
@@ -0,0 +1,55 @@
+/*
+ * Given the root of a binary tree, return the sum of all left leaves.
+ * 
+ * A leaf is a node with no children. A left leaf is a leaf that is the left
+ * child of another node.
+ *
+ * Example 1:
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 24
+ * Explanation: There are two left leaves in the binary tree, with values 9 and
+ * 15 respectively.
+ * 
+ * Example 2:
+ * Input: root = [1]
+ * Output: 0
+ * 
+ * Constraints:
+ * The number of nodes in the tree is in the range [1, 1000].
+ * -1000 <= Node.val <= 1000
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class SumOfLeftLeaves {
+    public static int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ans = 0;
+
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                ans += root.left.val;
+            } else {
+                ans += sumOfLeftLeaves(root.left);
+            }
+        }
+        ans += sumOfLeftLeaves(root.right);
+
+        return ans;
+    }
+}
