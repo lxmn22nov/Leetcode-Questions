39f78237b4e71be57c28c1432385125a846663ea
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-01 18:26:10 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/CheckIfArrayPairsAreDivisibleByK/CheckIfArrayPairsAreDivisibleByK.java
@@ -0,0 +1,45 @@
+package Medium.CheckIfArrayPairsAreDivisibleByK;
+
+/*
+Given an array of integers arr of even length n and an integer k.
+We want to divide the array into exactly n / 2 pairs such that the sum of each pair is divisible by k.
+Return true If you can find a way to do that or false otherwise.
+
+Example 1:
+Input: arr = [1,2,3,4,5,10,6,7,8,9], k = 5
+Output: true
+Explanation: Pairs are (1,9),(2,8),(3,7),(4,6) and (5,10).
+
+Example 2:
+Input: arr = [1,2,3,4,5,6], k = 7
+Output: true
+Explanation: Pairs are (1,6),(2,5) and(3,4).
+
+Example 3:
+Input: arr = [1,2,3,4,5,6], k = 10
+Output: false
+Explanation: You can try all possible pairs to see that there is no way to divide arr into 3 pairs each with sum divisible by 10.
+ */
+class CheckIfArrayPairsAreDivisibleByK {
+    public boolean canArrange(int[] arr, int k) {
+        int[] frequency = new int[k];
+
+        for (int num : arr) {
+            int remainder = num % k;
+
+            if (remainder < 0) {
+                remainder = remainder + k;
+            }
+            frequency[remainder]++;
+        }
+        if (frequency[0] % 2 != 0) {
+            return false;
+        }
+        for (int i = 1; i <= k / 2; i++) {
+            if (frequency[i] != frequency[k - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
