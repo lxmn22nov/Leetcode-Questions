eaced1817ac76efe392d4e2e4903eecace386608
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-14 07:38:42 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/MaximalScoreAfterApplyingKOperations/MaximalScoreAfterApplyingKOperations.java
@@ -0,0 +1,61 @@
+package Medium.MaximalScoreAfterApplyingKOperations;
+
+import java.util.*;
+
+/*
+ * PROBLEM: 2530
+ * 
+ * You are given a 0-indexed integer array nums and an integer k. You have a
+ * starting score of 0.
+ * 
+ * In one operation:
+ * 
+ * choose an index i such that 0 <= i < nums.length,
+ * increase your score by nums[i], and
+ * replace nums[i] with ceil(nums[i] / 3).
+ * Return the maximum possible score you can attain after applying exactly k
+ * operations.
+ * 
+ * The ceiling function ceil(val) is the least integer greater than or equal to
+ * val.
+ * 
+ * Example 1:
+ * Input: nums = [10,10,10,10,10], k = 5
+ * Output: 50
+ * Explanation: Apply the operation to each array element exactly once. The
+ * final score is 10 + 10 + 10 + 10 + 10 = 50.
+ * 
+ * Example 2:
+ * Input: nums = [1,10,3,3,3], k = 3
+ * Output: 17
+ * Explanation: You can do the following operations:
+ * Operation 1: Select i = 1, so nums becomes [1,4,3,3,3]. Your score increases
+ * by 10.
+ * Operation 2: Select i = 1, so nums becomes [1,2,3,3,3]. Your score increases
+ * by 4.
+ * Operation 3: Select i = 2, so nums becomes [1,1,1,3,3]. Your score increases
+ * by 3.
+ * The final score is 10 + 4 + 3 = 17.
+ */
+class MaximalScoreAfterApplyingKOperations {
+    public long maxKelements(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        // Add all elements to the priority queue.
+        for (int num : nums) {
+            pq.add(num);
+        }
+
+        int score = 0;
+
+        while (k > 0) {
+            // Extract the max element.
+            int element = pq.poll();
+            score += element;
+            // Re-insert ceil (element / 3)d into the queue.
+            pq.add((int) Math.ceil(element / 3.0));
+            k--;
+        }
+        return score;
+    }
+}
