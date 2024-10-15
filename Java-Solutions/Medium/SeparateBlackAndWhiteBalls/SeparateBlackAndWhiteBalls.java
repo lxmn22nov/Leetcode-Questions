5d48788d989df2b612c63d4c94a979311d3d3456
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-15 07:37:28 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/SeparateBlackAndWhiteBalls/SeparateBlackAndWhiteBalls.java
@@ -0,0 +1,57 @@
+package Medium.SeparateBlackAndWhiteBalls;
+
+/*
+ * PROBLEM: 2938
+ * 
+ * There are n balls on a table, each ball has a color black or white.
+ * 
+ * You are given a 0-indexed binary string s of length n, where 1 and 0
+ * represent black and white balls, respectively.
+ * 
+ * In each step, you can choose two adjacent balls and swap them.
+ * 
+ * Return the minimum number of steps to group all the black balls to the right
+ * and all the white balls to the left.
+ * 
+ * Example 1:
+ * Input: s = "101"
+ * Output: 1
+ * Explanation: We can group all the black balls to the right in the following
+ * way:
+ * - Swap s[0] and s[1], s = "011".
+ * Initially, 1s are not grouped together, requiring at least 1 step to group
+ * them to the right.
+ * 
+ * Example 2:
+ * Input: s = "100"
+ * Output: 2
+ * Explanation: We can group all the black balls to the right in the following
+ * way:
+ * - Swap s[0] and s[1], s = "010".
+ * - Swap s[1] and s[2], s = "001".
+ * It can be proven that the minimum number of steps needed is 2.
+ * 
+ * Example 3:
+ * Input: s = "0111"
+ * Output: 0
+ * Explanation: All the black balls are already grouped to the right.
+ */
+class SeparateBlackAndWhiteBalls {
+    public long minimumSteps(String s) {
+        long ans = 0;
+        // count the no.of black balls(1).
+        int blackBallCount = 0;
+
+        // traversing through the string.
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                // white ball encountered, add the no.of black balls on its left.
+                ans += blackBallCount;
+            } else {
+                // black ball encountered, increment the black ball count.
+                blackBallCount++;
+            }
+        }
+        return ans;
+    }
+}
