9f37725ac3df1628afb0480f4f05460940e9264b
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-18 16:36:35 +0530

    Added question to Medium.

---
+++ Java-Solutions/Medium/CountNumberOfMaximumBitwiseORsubsets/CountNumberOfMaximumBitwiseORsubsets.java
@@ -0,0 +1,32 @@
+package Medium.CountNumberOfMaximumBitwiseORsubsets;
+
+/*
+ * PROBLEM: 2044: Count Number of Maximum Bitwise-OR Subsets.
+ */
+
+class Solution {
+    public void backtrack(int[] nums, int index, int currentOR, int maxOR, int[] count) {
+        if (currentOR == maxOR) {
+            count[0]++;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            backtrack(nums, i + 1, currentOR | nums[i], maxOR, count);
+        }
+    }
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOR = 0;
+
+        // Step 1: Compute the maximum OR
+        for (int num : nums) {
+            maxOR |= num;
+        }
+
+        int[] count = new int[1];
+        // Step 2: Backtrack to count the subsets
+        backtrack(nums, 0, 0, maxOR, count);
+
+        return count[0];
+    }
+}
